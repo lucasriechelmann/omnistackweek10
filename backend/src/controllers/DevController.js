e3c7df54aa89309a1ff4e4e8d8399b92f4f74503
Lucas Riechelmann Ramos <lucas.riechelmann@hotmail.com>
2020-03-28 15:57:32 +0000

    Implemented socket.io to real connection between mobile and backend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
     async index(request, response){
@@ -29,6 +30,9 @@ module.exports = {
                 techs: techsArray,
                 location
             });
+
+            const sendSocketMessageTo = findConnections({ latitude, longitude }, techsArray);
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
         return response.json(dev);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
 
 mongoose.connect('mongodb+srv://lucas:lucas@cluster0-okyjv.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+setupWebsocket(server);
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+server.listen(3333);
